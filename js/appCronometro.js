bc1c2be8e94b918ae70c73c0366761064d3159bf
Ferdzen <fernandaivp63@gmail.com>
2025-05-15 20:48:40 -0300

    add: layout cronometro

---
+++ js/appCronometro.js
@@ -0,0 +1,5 @@
+const botaoIniciar = document.querySelector("#botao-iniciar");
+const botaoPausar = document.querySelector("#botao-pausar");
+const botaoReiniciar = document.querySelector("#botao-reiniciar");
+
+
